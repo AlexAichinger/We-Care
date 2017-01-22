73edd3d4bb74476ab3ac171bc37f17b917c91cfb
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-01-21 20:16:03 -0800

    Convert code to Python 2.7

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
 from flask import Flask, render_template, request, jsonify
+#from rauth import OAuth2Service
 import os
+from fb import getFriends
 
 app = Flask(__name__)
+access_token = ""
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/authenticate-facebook', methods=["GET"])
-def authenticate():
+def authenticate_facebook():
+    '''
+    FB_CLIENT_ID = '769684356512385'
+    FB_CLIENT_SECRET = 'bfafc3ab14067170a74e3977643584f6'
 
+    graph_url = 'https://graph.facebook.com/'
+    facebook = OAuth2Service(name='facebook',
+        authorize_url='https://www.facebook.com/dialog/oauth',
+        access_token_url=graph_url + 'oauth/access_token',
+        client_id=app.config['FB_CLIENT_ID'],
+        client_secret=app.config['FB_CLIENT_SECRET'],
+        base_url=graph_url)
+    redirect_url = url_for('authorized', _external=True)
+    params = {'redirect_url': redirect_url}
+    access_token = facebook.get_authorize_url(**params)
+    '''
     return jsonify(success=True)
-
-@app.route('/submit', methods=["POST"])
-def submit():
-
-    return jsonify(success=True, data=data)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,38 @@
+console.log("hey")
 $(document).ready(function() {
-  
+  $('#authenticate-facebook').on("click", function(evt) {
+    controllers.authenticate_facebook();
+  });
+  $('#submit').on("click", function(evt) {
+    var facebook = $('#facebook').val();
+    var twitter = $('#twitter').val();
+    var instagram = $('#instagram').val();
+    controllers.submit(facebook, twitter, instagram);
+  });
 });
+
+var controllers = {
+  authenticate_facebook: function() {
+    $.get({
+      url: '/authenticate-facebook'
+    }.done(function(data) {
+      if (data.success) {
+
+      } else {
+
+      }
+    }).fail(function(data) {
+
+    }));
+  },
+  submit: function(facebook, twitter, instagram) {
+    $.post({
+      url: '/submit',
+      data: [facebook, twitter, instagram]
+    }.done(function(data) {
+
+    }).fail(function(data) {
+
+    }));
+  }
+};
