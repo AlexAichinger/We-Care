c5e443fdaf19a618a02136d7aa6145bac4478d41
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-01-22 01:54:03 -0800

    Add value to front end

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
-console.log("hey")
 $(document).ready(function() {
+  $('#value').css("visibility", "hidden");
   $('#authenticate-facebook').on("click", function(evt) {
     controllers.authenticate_facebook();
   });
@@ -9,30 +9,49 @@ $(document).ready(function() {
     var instagram = $('#instagram').val();
     controllers.submit(facebook, twitter, instagram);
   });
+  display.displayValue(500);
 });
 
 var controllers = {
   authenticate_facebook: function() {
     $.get({
       url: '/authenticate-facebook'
-    }.done(function(data) {
+    }).done(function(data) {
       if (data.success) {
-
+        display.displayValue(9);
+        console.log(data.url);
       } else {
 
       }
     }).fail(function(data) {
 
-    }));
+    });
   },
   submit: function(facebook, twitter, instagram) {
     $.post({
       url: '/submit',
       data: [facebook, twitter, instagram]
-    }.done(function(data) {
+    }).done(function(data) {
+      if (data.success) {
+        display.displayValue(500);
+      } else {
 
+      }
     }).fail(function(data) {
 
-    }));
+    });
+  }
+};
+
+var display = {
+  scrollToBottom: function() {
+    window.scrollTo(0, document.body.scrollHeight);
+  },
+  displayValue: function(value) {
+    var element = $('#value');
+    $('#value span').html(value);
+    element.css("visibility", "visible");
+    element.addClass('valign-wrapper');
+    display.scrollToBottom();
   }
 };
