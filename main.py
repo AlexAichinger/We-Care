76dc21cee522b9519edf85a41ba1e8ab58bb545c
amitSA <send.amit@yahoo.com>
2017-01-22 01:50:34 -0800

    removed the static files to the directories Thomas orriginaly put them in

--- main.py
+++ main.py
@@ -16,3 +16,8 @@ def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
+
+	
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
